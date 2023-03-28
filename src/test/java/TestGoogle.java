82e8dd217ed41e78e86b6ab7ff57029e1260b4f1
Hocorend <hocorend@gmail.com>
2023-03-28 19:41:56 +0500

    test

--- src/test/java/TestGoogle.java
+++ src/test/java/TestGoogle.java
@@ -1,11 +1,22 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestGoogle {
 
     public void testGoogle() throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+        Thread.sleep(5000);
+
+        driver.quit();
+
+    }
+
+    public void  testFirefox() throws InterruptedException{
+        WebDriver driver = new FirefoxDriver();
         driver.get("https://google.com");
 
         Thread.sleep(5000);

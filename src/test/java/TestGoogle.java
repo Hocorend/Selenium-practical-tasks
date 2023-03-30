c71c0720e373b0b4601ab09a01b0587f41782de3
Hocorend <hocorend@gmail.com>
2023-03-30 16:42:01 +0500

    test

--- src/test/java/TestGoogle.java
+++ src/test/java/TestGoogle.java
@@ -1,8 +1,14 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class TestGoogle {
 
@@ -11,9 +17,9 @@ public class TestGoogle {
         WebDriver driver = new ChromeDriver();
         driver.get("https://google.com");
 
-        driver.findElement(By.xpath("//input[@title='Поиск']")).sendKeys("Авиационные реактивные двигатели");
+        waitForElementLocatedBy(driver,Duration.ofSeconds(10),By.xpath("//input[@title='Поиск']")).sendKeys("Авиационные реактивные двигатели");;
 
-        driver.findElement(By.xpath("//input[@value ='Поиск в Google' and @role='button']")).submit();
+        waitForElementLocatedBy(driver,Duration.ofSeconds(10),By.xpath("//input[@value ='Поиск в Google' and @role='button']")).submit();
 
         Thread.sleep(5000);
 
@@ -21,27 +27,8 @@ public class TestGoogle {
 
     }
 
-    public void  testEdge() throws InterruptedException{
-        WebDriver driver = new EdgeDriver();
-        driver.get("https://google.com");
-        driver.findElement(By.xpath("//input[@title='Поиск']")).sendKeys("Авиационные реактивные двигатели");
-
-        driver.findElement(By.xpath("//input[@value ='Поиск в Google' and @role='button']")).submit();
-
-        Thread.sleep(5000);
-
-        driver.quit();
-    }
-
-    public void  testFirefox() throws InterruptedException{
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://google.com");
-        driver.findElement(By.xpath("//input[@title='Поиск']")).sendKeys("Авиационные реактивные двигатели");
-
-        driver.findElement(By.xpath("//input[@value ='Поиск в Google' and @role='button']")).submit();
-
-        Thread.sleep(5000);
-
-        driver.quit();
+    private static WebElement waitForElementLocatedBy(WebDriver driver,Duration duration, By by) {
+        return new WebDriverWait(driver, duration)
+                .until(ExpectedConditions.presenceOfElementLocated(by));
     }
 }
